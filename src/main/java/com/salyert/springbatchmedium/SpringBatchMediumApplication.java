4a7d8db3d0440b67d869b42b43dcb58cae3ec723
chandra swarathesh <swarathesh60@gmail.com>
2021-04-09 10:36:33 -0400

    Initial commit

---
+++ src/main/java/com/salyert/springbatchmedium/SpringBatchMediumApplication.java
@@ -0,0 +1,13 @@
+package com.salyert.springbatchmedium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMediumApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(SpringBatchMediumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salyert/springbatchmedium/SpringBatchMediumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salyert.springbatchmedium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMediumApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
